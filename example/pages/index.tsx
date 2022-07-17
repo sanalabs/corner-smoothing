bbbdeb709e43da98a400ed374ef5f2d1d74a66eb
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2022-07-17 02:17:40 +0200

    wip

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -2,61 +2,196 @@ import { useState } from "react";
 import { SquircleTodo, squircle } from "squircle-todo";
 import styled from "styled-components";
 
-const Button = styled.div`
+// Styling a Squircle can be done in any way, it doesn't have to be with
+// styled-components. This is just to demonstrate.
+const FilledBox = styled.div`
   background-color: #7c3aed;
   color: white;
-  width: 10%;
-  padding: 1rem;
-  height: 100px;
+  width: max(20%, 150px);
+  padding: 0.5rem;
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  :hover {
+    background-color: #8b5cf6;
+  }
+`;
+
+const Row = styled.div`
   margin: 1rem auto;
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 1rem;
+`;
 
-  :hover {
+const BorderBoxNormal = styled.div`
+  background-color: #fff;
+  color: #333;
+  width: max(20%, 150px);
+  height: 75px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border-color: #8b5cf6;
+  border-style: solid;
+`;
+
+const BorderBox = styled.div`
+  background-color: #7c3aed;
+  color: #333;
+  width: max(20%, 150px);
+  height: 75px;
+
+  .squircle-inner {
+    background-color: #fff;
+    padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
+  :hover .squircle-inner {
     background-color: #8b5cf6;
-    cursor: pointer;
+    color: white;
   }
 `;
 
-export default function Page() {
-  const [radius, setRadius] = useState(10)
-  const [smoothing100, setSmoothing100] = useState(60)
-  const smoothing = smoothing100 / 100
 
-  const SquircleButton = squircle(Button, { cornerRadius: radius, cornerSmoothing: smoothing });
+const StyledContainer = styled.div`
+  background-color: red;
+  display: inline-block;
+
+  .squircle-inner {
+    background-color: #fff;
+    padding: 0.5rem;
+  }
+`
+
+const Container = squircle(StyledContainer, {
+  cornerRadius: 25,
+  borderWidthPx: 1
+});
+
+
+export default function Page() {
+  const [radiusPx, setRadiusPx] = useState(25);
+  const [smoothing100, setSmoothing100] = useState(100);
+  const [borderWidthPx2, setBorderWidthPx2] = useState(2);
+
+  const borderWidthPx = borderWidthPx2 / 2;
+  const smoothing = smoothing100 / 100;
 
   return (
-    <div>
-      <input type="range" min={1} max={100} value={radius} onChange={e => setRadius(parseInt(e.target.value))} /> {radius}px
-      <input type="range" min={0} max={100} value={smoothing100} onChange={e => setSmoothing100(parseInt(e.target.value))} /> {smoothing}
+    <Container>
+      <table style={{ margin: '1rem auto 2rem' }}>
+        <tbody>
+          <tr>
+            <td style={{ textAlign: 'right' }}>Radius:</td>
+            <td>
+              <input
+                type="range"
+                min={1}
+                max={50}
+                value={radiusPx}
+                onChange={(e) => setRadiusPx(parseInt(e.target.value))}
+              />
+            </td>
+            <td style={{ width: '50px' }}>{radiusPx} px</td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'right' }}>Smoothing:</td>
+            <td>
+              <input
+                type="range"
+                min={0}
+                max={100}
+                value={smoothing100}
+                onChange={(e) => setSmoothing100(parseInt(e.target.value))}
+              />
+            </td>
+            <td style={{ width: '50px' }}>{smoothing}</td>
+          </tr>
+          <tr>
+            <td>Border width:</td>
+            <td>
+              <input
+                type="range"
+                min={1}
+                max={50}
+                value={borderWidthPx2}
+                onChange={(e) => setBorderWidthPx2(parseInt(e.target.value))}
+              />
+            </td>
+            <td style={{ width: '50px' }}>{borderWidthPx} px</td>
+          </tr>
+        </tbody>
+      </table>
+      <Row>
+        <div
+          style={{
+            width: "max(20%, 150px)",
+            textAlign: "center",
+            fontWeight: 500,
+          }}
+        >
+          Before:
+        </div>
+        <div
+          style={{
+            width: "max(20%, 150px)",
+            textAlign: "center",
+            fontWeight: 500,
+          }}
+        >
+          After:
+        </div>
+      </Row>
+      <Row>
+        <FilledBox style={{ borderRadius: `${radiusPx}px` }}>
+          Filled
+          <br />
+          Normal
+        </FilledBox>
+        <SquircleTodo
+          cornerRadius={radiusPx}
+          cornerSmoothing={smoothing}
+          as={FilledBox}
+        >
+          Filled
+          <br />
+          Squircle
+        </SquircleTodo>{" "}
+      </Row>
+      <Row>
+        <BorderBoxNormal
+          style={{
+            borderRadius: `${radiusPx}px`,
+            borderWidth: `${borderWidthPx}px`,
+          }}
+        >
+          Border
+          <br />
+          Normal
+        </BorderBoxNormal>
 
-      <Button style={{ borderRadius: `${radius}px` }}>Normal border radius</Button>
-
-      <SquircleButton>Wrapped in squircle HOC</SquircleButton>
-
-      <SquircleTodo cornerRadius={radius} cornerSmoothing={smoothing} as={Button}>
-        Wrapped in Squircle component with as={`{Button}`}
-      </SquircleTodo>
-
-      {/* <SquircleTodo
-        cornerRadius={radius}
-        cornerSmoothing={smoothing}
-        as="span"
-        style={{
-          backgroundColor: "#7c3aed",
-          color: 'white',
-          width: "10%",
-          padding: '1rem',
-          height: '100px',
-          margin: "1rem auto",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Explicitly styled Squircle component
-      </SquircleTodo> */}
-    </div>
+        <SquircleTodo
+          cornerRadius={radiusPx}
+          cornerSmoothing={smoothing}
+          borderWidthPx={borderWidthPx}
+          as={BorderBox}
+        >
+          Border
+          <br />
+          Squircle
+        </SquircleTodo>
+      </Row>
+    </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,35 +7,145 @@ import React, {
   ReactElement,
   ReactHTML,
   ReactNode,
-  useEffect,
   useRef,
 } from "react";
 import { getSvgPath, FigmaSquircleParams } from "figma-squircle";
 
 type Require<T, K extends keyof T> = T & { [P in K]-?: T[P] };
+type Optional<T, K extends keyof T> = Omit<T, K> & { [P in K]?: T[P] };
 
-type Options = Require<
-  Omit<FigmaSquircleParams, "width" | "height">,
-  "cornerRadius"
->;
+type Options = Optional<
+  Require<Omit<FigmaSquircleParams, "width" | "height">, "cornerRadius">,
+  "cornerSmoothing"
+> & { borderWidthPx?: number };
 
-const vanillaSquircle = (el: HTMLElement, opts: Options) => {
+/** CSS id must start with a letter. */
+const createCssId = (length: number = 8): string => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return `squircle-${result}`;
+};
+
+const setCss = (css: string, el: HTMLElement, id: string): void => {
+  const styleId = `style-${id}`;
+
+  let styleEl = document.getElementById(styleId);
+
+  if (!styleEl) {
+    styleEl = document.createElement("style");
+    styleEl.id = styleId;
+    document.head.appendChild(styleEl);
+  }
+
+  styleEl.textContent = css.replace(/&/g, `.${id}`);
+};
+
+function asNonNullable<T>(val: T): NonNullable<T> {
+  if (val === undefined || val === null)
+    throw new Error("Expeced non-nullable");
+  return val as NonNullable<T>;
+}
+
+const getContentWidth = (element: HTMLElement): number => {
+  const computedStyle = getComputedStyle(element);
+  return (
+    element.clientWidth -
+    parseFloat(computedStyle.paddingLeft) -
+    parseFloat(computedStyle.paddingRight)
+  );
+};
+
+const getContentHeight = (element: HTMLElement): number => {
+  const computedStyle = getComputedStyle(element);
+  return (
+    element.clientHeight -
+    parseFloat(computedStyle.paddingTop) -
+    parseFloat(computedStyle.paddingBottom)
+  );
+};
+
+const vanillaSquircle = (el: HTMLElement, opts: Options, id: string): void => {
+  el.style.clipPath = 'unset';
+
   const width = el.clientWidth;
   const height = el.clientHeight;
+  // const width = getContentWidth(el);
+  // const height = getContentHeight(el);
+
   const opts2 = {
     preserveSmoothing: true, // Default to true, contrary to the default of figma-squircle.
+    cornerSmoothing: 1,
     width,
     height,
     ...opts,
+  };
+
+  console.log("vanillaSquircle", width, height);
+
+  if (!opts.borderWidthPx) {
+    el.style.clipPath = `path('${getSvgPath(opts2)}')`;
+  } else {
+    const firstRender = !el.classList.contains(id);
+    let innerEl: HTMLElement;
+
+    if (firstRender) {
+      el.classList.add(id);
+      innerEl = document.createElement("div");
+      innerEl.classList.add(`squircle-inner`);
+      innerEl.append(...el.childNodes);
+      el.append(innerEl);
+    } else {
+      innerEl = asNonNullable(el.querySelector(".squircle-inner"));
+    }
+
+    Object.assign(el.style, {
+      position: "relative",
+      clipPath: `path('${getSvgPath(opts2)}')`,
+    });
+
+    // TODO: Move to ::before approach to avoid the problems that come with
+    // nesting another div. Namely, difficulty to adjust to content dimensions
+    // and the drag handle loses focus.
+    
+    Object.assign(innerEl.style, {
+      // position: "absolute",
+      // inset: `${opts.borderWidthPx}px`,
+      width: `${getContentWidth(el) - opts.borderWidthPx * 2}px`,
+      height: `${getContentHeight(el) - opts.borderWidthPx * 2}px`,
+      margin: `${opts.borderWidthPx}px`,
+      clipPath: `path('${getSvgPath({
+        ...opts2,
+        width: width - opts.borderWidthPx * 2,
+        height: height - opts.borderWidthPx * 2,
+        cornerRadius: opts2.cornerRadius - opts.borderWidthPx,
+      })}')`,
+    });
+
+    // setCss(`
+    //   & {
+    //     position: relative;
+    //     clip-path: path('${getSvgPath(opts2)}');
+    //   }
+
+    //   & > div {
+    //     position: absolute;
+    //     background-color: white;
+    //     clip-path: path('${getSvgPath({ ...opts2, width: width - opts.borderWidthPx * 2, height: height - opts.borderWidthPx * 2, cornerRadius: opts2.cornerRadius - opts.borderWidthPx })}');
+    //     inset: ${opts.borderWidthPx}px;
+    //     display: block;
+    //   }
+    // `, el, id)
   }
-  console.log('vanillaSquircle', opts2, opts)
-  const path = getSvgPath(opts2);
-  el.style.clipPath = `path('${path}')`;
 };
 
 const squircleObserver = (el: HTMLElement, opts: Options) => {
-  console.log('squircleObserver init')
-  
+  console.log("squircleObserver init");
+
+  const id = createCssId();
+
   // Initialize as `undefined` to always run directly when instantiating.
   let dimensions: [number, number] | undefined = undefined;
 
@@ -44,12 +154,12 @@ const squircleObserver = (el: HTMLElement, opts: Options) => {
       opts = newOpts;
     }
 
-    return vanillaSquircle(el, opts);
+    return vanillaSquircle(el, opts, id);
   };
 
   const resizeObserver = new ResizeObserver(() => {
-    console.log('squircleObserver observing')
-    
+    console.log("squircleObserver observing");
+
     const prevDimemsions = dimensions;
     dimensions = [el.clientWidth, el.clientHeight];
 
@@ -68,7 +178,17 @@ const squircleObserver = (el: HTMLElement, opts: Options) => {
   // The native code `resizeObserver.disconnect` needs the correct context.
   // Retain the context by wrapping in arrow function. Read more about this:
   // https://stackoverflow.com/a/9678166/19306180
-  func.disconnect = () => resizeObserver.disconnect();
+  func.disconnect = () => {
+    el.classList.remove(id);
+
+    const innerEl = el.querySelector(".squircle-inner");
+    if (innerEl) {
+      el.append(...innerEl.childNodes);
+      innerEl.remove();
+    }
+
+    resizeObserver.disconnect();
+  };
 
   return func;
 };
@@ -78,6 +198,21 @@ type Props = {
   as?: keyof ReactHTML | ComponentType<any>;
 } & Options &
   DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+const omitUndefined = <T,>(
+  obj: T
+): { [key in keyof T]: Exclude<T[key], undefined> } => {
+  const result = {} as { [key in keyof T]: Exclude<T[key], undefined> };
+
+  for (const key in obj) {
+    if (obj[key] !== undefined) {
+      // @ts-ignore
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
+};
 
 export const SquircleTodo = forwardRef(
   (
@@ -91,11 +226,12 @@ export const SquircleTodo = forwardRef(
       bottomLeftCornerRadius,
       cornerSmoothing,
       preserveSmoothing,
+      borderWidthPx,
       ...rest
     }: PropsWithoutRef<Props>,
     forwardedRef
   ): ReactElement => {
-    const opts = {
+    const opts = omitUndefined({
       cornerRadius,
       topLeftCornerRadius,
       topRightCornerRadius,
@@ -103,13 +239,7 @@ export const SquircleTodo = forwardRef(
       bottomLeftCornerRadius,
       cornerSmoothing,
       preserveSmoothing,
-    };
-
-    // Remove keys with value `undefined` since they may receive default
-    ;(Object.keys(opts) as (keyof typeof opts)[]).forEach((key) => {
-      if (opts[key] === undefined) {
-        delete opts[key];
-      }
+      borderWidthPx,
     });
 
     const funcRef = useRef<ReturnType<typeof squircleObserver>>();
@@ -192,3 +322,22 @@ export const squircle = <P,>(
       />
     );
   });
+
+// /**
+//  * HOC that wraps `Component` and injects the squircle style.
+//  */
+// export const squircleBorder = <P extends { children: ReactElement },>(
+//   Component: keyof ReactHTML | ComponentType<P>,
+//   opts: Options & { borderWidth: string }
+// ) =>
+//   forwardRef(({ children, ...props }: P, ref): ReactElement => {
+//     const Outer = squircle('div', opts);
+//     const Inner = squircle(Component, { ...opts });
+
+//     return (
+//       <Outer {...props} ref={ref} >
+//         {/* @ts-ignore */}
+//         <Inner>{children}</Inner>
+//       </Outer>
+//     );
+//   });
